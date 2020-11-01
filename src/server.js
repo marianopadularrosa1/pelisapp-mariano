2a18def82331b96681f7e847ec0300e91e76d0e8
Mariano <mariano.padularrosa@gmail.com>
2020-11-01 00:38:52 -0300

    deploy heroku

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
